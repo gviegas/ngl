1687e57ef2ff43cb13159f39f7d0629d6168d29c
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-10-14 16:08:36 -0300

    Add shared Context for testing

--- src/test/test.zig
+++ src/test/test.zig
@@ -1,6 +1,24 @@
 const std = @import("std");
 const testing = std.testing;
+pub const allocator = testing.allocator;
+
+const ngl = @import("../ngl.zig");
 
 test {
     // TODO
+    context().deinit(allocator);
+}
+
+pub fn context() *ngl.Context {
+    const Static = struct {
+        var ctx: ngl.Context = undefined;
+        var once = std.once(init);
+
+        fn init() void {
+            ctx = ngl.Context.initDefault(allocator) catch |err| @panic(@errorName(err));
+        }
+    };
+
+    Static.once.call();
+    return &Static.ctx;
 }
