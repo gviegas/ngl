286766c8c0744e60ec9aa960fceebb52c732e07f
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-10-15 18:16:01 -0300

    Add Semaphore test

---
+++ src/test/sema.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const testing = std.testing;
+
+const ngl = @import("../ngl.zig");
+const gpa = @import("test.zig").gpa;
+const context = @import("test.zig").context;
+
+test "Semaphore.init/deinit" {
+    const dev = &context().device;
+
+    // Only vanilla (binary) semaphore is supported currently
+    var sema = try ngl.Semaphore.init(gpa, dev, .{});
+    sema.deinit(gpa, dev);
+}
--- src/test/test.zig
+++ src/test/test.zig
@@ -24,4 +24,5 @@ test {
     _ = @import("inst.zig");
     _ = @import("dev.zig");
     _ = @import("fence.zig");
+    _ = @import("sema.zig");
 }
