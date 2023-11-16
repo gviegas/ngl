3ab2c688d1e4a0e1ce7cda20104560e2070be94d
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-11-16 10:44:16 -0300

    Lock queue/platform in Queue.present test

--- src/test/queue.zig
+++ src/test/queue.zig
@@ -6,6 +6,7 @@ const gpa = @import("test.zig").gpa;
 const context = @import("test.zig").context;
 const queue_locks = &@import("test.zig").queue_locks;
 const platform = @import("sf.zig").platform;
+const platform_lock = &@import("sf.zig").platform_lock;
 
 test "Queue.submit" {
     const dev = &context().device;
@@ -219,6 +220,11 @@ test "Queue.present" {
         .count = @intCast(plat.images.len),
     });
     defer gpa.free(cmd_bufs);
+
+    queue_locks[plat.queue_index].lock();
+    defer queue_locks[plat.queue_index].unlock();
+    platform_lock.lock();
+    defer platform_lock.unlock();
 
     for (0..plat.images.len) |i| {
         const next = try plat.swap_chain.nextImage(dev, timeout, null, &fences[i]);
