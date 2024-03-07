ffff7cee4c78050fce5b407d073dcdef49041692
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2024-03-07 18:34:06 -0300

    Update inc.h

--- ngl/src/inc.h
+++ ngl/src/inc.h
@@ -1,18 +1,24 @@
-#if defined(__linux__) || defined(_WIN32) || defined(__ANDROID__)
-# define VK_NO_PROTOTYPES
-# include <vulkan/vulkan_core.h>
+#if defined(__linux__) || defined(_WIN32) // || defined(__ANDROID__)
+#define VK_NO_PROTOTYPES
+#include <vulkan/vulkan_core.h>
+#elif defined(__APPLE__)
+#error Not yet implemented
 #else
-# error Not supported
+#error Not supported
 #endif
 
 #if defined(__ANDROID__)
+// TODO
 #elif defined(__linux__)
-# include <wayland-client.h>
-# include <vulkan/vulkan_wayland.h>
-# include <xcb/xcb.h>
-# include <vulkan/vulkan_xcb.h>
-# include <dlfcn.h>
+#include <wayland-client.h>
+#include <vulkan/vulkan_wayland.h>
+#include <xcb/xcb.h>
+#include <vulkan/vulkan_xcb.h>
+#include <dlfcn.h>
 #elif defined(_WIN32)
-# include <windows.h>
-# include <vulkan/vulkan_win32.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <vulkan/vulkan_win32.h>
+#elif defined(__APPLE__)
+// TODO
 #endif
