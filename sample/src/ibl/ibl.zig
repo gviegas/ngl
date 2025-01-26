e2ee4e3570c08b7e8bae6037081dc4586d420dd6
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2025-01-26 11:37:51 -0300

    IBL sample: sRGB clear color

--- sample/src/ibl/ibl.zig
+++ sample/src/ibl/ibl.zig
@@ -604,6 +604,12 @@ fn do(gpa: std.mem.Allocator) !void {
     var timer = try std.time.Timer.start();
     var prev_input = pfm.Platform.Input{};
 
+    const clr_col: ngl.Cmd.ClearValue = blk: {
+        const lin = 0.75;
+        const ch = if (plat.format.format.isSrgb()) lin else std.math.pow(f32, lin, 1.0 / 2.0);
+        break :blk .{ .color_f32 = .{ ch, ch, ch, 1 } };
+    };
+
     while (true) {
         const dt: f32 = @floatCast(@as(f64, @floatFromInt(timer.lap())) / std.time.ns_per_s);
 
@@ -767,7 +773,7 @@ fn do(gpa: std.mem.Allocator) !void {
                 .layout = .color_attachment_optimal,
                 .load_op = .clear,
                 .store_op = .dont_care,
-                .clear_value = .{ .color_f32 = .{ 0.5, 0.5, 0.5, 1 } },
+                .clear_value = clr_col,
                 .resolve = .{
                     .view = &col_s1.view,
                     .layout = .color_attachment_optimal,
