08a3d72ab42bcfe06ea8645e32f833c6b95b9b6e
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2025-01-26 11:32:51 -0300

    SSAO sample: sRGB clear color

--- sample/src/ssao/ssao.zig
+++ sample/src/ssao/ssao.zig
@@ -344,6 +344,12 @@ fn do(gpa: std.mem.Allocator) !void {
         }});
     }
 
+    const clr_col: ngl.Cmd.ClearValue = blk: {
+        const lin = 0.75;
+        const ch = if (plat.format.format.isSrgb()) lin else std.math.pow(f32, lin, 1.0 / 2.2);
+        break :blk .{ .color_f32 = .{ ch, ch, ch, 1 } };
+    };
+
     while (true) {
         const input = plat.poll();
         if (input.done)
@@ -519,7 +525,7 @@ fn do(gpa: std.mem.Allocator) !void {
                     .layout = .color_attachment_optimal,
                     .load_op = .clear,
                     .store_op = .dont_care,
-                    .clear_value = .{ .color_f32 = .{ 0.5, 0.5, 0.5, 1 } },
+                    .clear_value = clr_col,
                     .resolve = .{
                         .view = &col_s1.view,
                         .layout = .color_attachment_optimal,
