c04592cd9cd776270fd709a06126e47542f83239
Gustavo Viegas <20372236+gviegas@users.noreply.github.com>
2025-05-16 16:32:43 -0300

    Use pre-translated code in Windows x86_64

--- ngl/src/inc.zig
+++ ngl/src/inc.zig
@@ -1,29 +1,20 @@
 const builtin = @import("builtin");
 
-pub usingnamespace @cImport({
-    switch (builtin.mode) {
-        .ReleaseFast, .ReleaseSmall => @cDefine("NDEBUG", {}),
-        else => {},
-    }
-    switch (builtin.os.tag) {
-        .linux, .windows => {
-            @cDefine("VK_NO_PROTOTYPES", {});
-            @cInclude("vulkan/vulkan_core.h");
-        },
-        else => {},
-    }
-    switch (builtin.os.tag) {
-        .linux => if (!builtin.abi.isAndroid()) {
-            @cInclude("dlfcn.h");
-            @cInclude("wayland-client.h");
-            @cInclude("xdg-shell-client.h");
-            @cInclude("vulkan/vulkan_wayland.h");
-        },
-        .windows => {
-            @cDefine("UNICODE", {});
-            @cInclude("windows.h");
-            @cInclude("vulkan/vulkan_win32.h");
-        },
-        else => @compileError("TODO"),
-    }
-});
+pub usingnamespace if (builtin.os.tag == .windows and builtin.cpu.arch == .x86_64)
+    @import("windows.zig")
+else
+    @cImport({
+        switch (builtin.os.tag) {
+            .linux => {
+                @cDefine("VK_NO_PROTOTYPES", {});
+                @cInclude("vulkan/vulkan_core.h");
+                if (!builtin.abi.isAndroid()) {
+                    @cInclude("dlfcn.h");
+                    @cInclude("wayland-client.h");
+                    @cInclude("xdg-shell-client.h");
+                    @cInclude("vulkan/vulkan_wayland.h");
+                }
+            },
+            else => @compileError("TODO"),
+        }
+    });
