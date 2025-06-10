8a3448cef28e5b5795656a32eb44dbcbdf9f4bd1
Gustavo Viegas <20372236+gviegas@users.noreply.github.com>
2025-06-10 10:18:58 -0300

    Fix missing commands in test color blending

--- ngl/src/test/blend.zig
+++ ngl/src/test/blend.zig
@@ -411,6 +411,9 @@ const T = struct {
         cmd.setFrontFace(triangle.front_face);
         cmd.setSampleCount(.@"1");
         cmd.setSampleMask(0b1);
+        cmd.setAlphaToCoverageEnable(false);
+        cmd.setAlphaToOneEnable(false);
+        cmd.setDepthClampEnable(false);
         cmd.setDepthBiasEnable(false);
         cmd.setDepthTestEnable(false);
         cmd.setDepthWriteEnable(false);
